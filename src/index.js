1db176881f3347dc67eb6d713b461adf59530484
Guan Gui <guan@southbanksoftware.com>
2017-12-20 15:52:14 +1100

    Minor fix to export

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: guiguan
  * @Date:   2017-03-29T12:00:46+11:00
  * @Last modified by:   guiguan
- * @Last modified time: 2017-03-29T14:14:19+11:00
+ * @Last modified time: 2017-12-20T15:51:13+11:00
  */
 
-export {dump, restore} from './dumpenvy';
+export { dump, restore, nodump } from './dumpenvy';
